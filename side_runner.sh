3c1eadada7c2770f3ab77b5e252f6701a0c694f2
ChillerDragon <ChillerDragon@gmail.com>
2024-03-05 20:07:48 +0800

    Fix it not shutting down on plugin shutdown
    Killing the side_runner.sh did not kill the frontail thing

--- side_runner.sh
+++ side_runner.sh
@@ -9,6 +9,19 @@ fi
 
 source lib/lib.sh
 
+if [ ! -x "$(command -v frontail)" ]
+then
+	export NVM_DIR="$HOME/.nvm"
+	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+	[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
+if [ ! -x "$(command -v frontail)" ]
+then
+	echo "Error: please run $(tput bold)npm i frontail -g$(tput sgr0)"
+	exit 1
+fi
+
+
 echo "*** server-plugin-frontail ***"
 echo "starting side runner with the following config:"
 echo "url=$CFG_PL_FRONTAIL_URL"
@@ -16,5 +29,8 @@ echo "url=$CFG_PL_FRONTAIL_URL"
 # give the server time to start
 sleep 10
 
-./lib/plugins/server-plugin-frontail/lib/main.sh
+# plugins get restarted on server restart
+# so this logfile should always be current
+# as long as the server does not rotate logs without restart
 
+frontail -h 127.0.0.1 --url-path /frontail --disable-usage-stats "$(./show_log.sh --filepath)"
